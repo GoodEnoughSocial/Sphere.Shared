be8a8ab2603042753ad9822acde51bf2312cede9
Chad Jessup <chad.jessup@gmail.com>
2022-06-03 00:41:54 -0700

    more helpers - logger and service reg

--- Sphere.Shared/Services.cs
+++ Sphere.Shared/Services.cs
@@ -1,5 +1,4 @@
 ﻿using Consul;
-
 using Flurl;
 
 namespace Sphere.Shared;
@@ -7,9 +6,38 @@ namespace Sphere.Shared;
 public static class Services
 {
     // TODO: make data driven, or dynamic...
-    public static readonly ServiceDefinition Administration = new("Administration", "https", "localhost", 5002);
-    public static readonly ServiceDefinition Auth = new("Auth", "https", "localhost", 5004);
-    public static readonly ServiceDefinition SphereWebTestApp = new("WebTestApp", "https", "localhost", 5013);
+    public static readonly ServiceDefinition Accounts = new(nameof(Accounts), "https", "localhost", 5001);
+    public static readonly ServiceDefinition Administration = new(nameof(Administration), "https", "localhost", 5002);
+    public static readonly ServiceDefinition ApiGateway = new(nameof(ApiGateway), "https", "localhost", 5003);
+    public static readonly ServiceDefinition Auth = new(nameof(Auth), "https", "localhost", 5004);
+    public static readonly ServiceDefinition Branding = new(nameof(Branding), "https", "localhost", 5005);
+    public static readonly ServiceDefinition Health = new(nameof(Health), "https", "localhost", 5006);
+    public static readonly ServiceDefinition Media = new(nameof(Media), "https", "localhost", 5007);
+    public static readonly ServiceDefinition Messages = new(nameof(Messages), "https", "localhost", 5008);
+    public static readonly ServiceDefinition Profiles = new(nameof(Profiles), "https", "localhost", 5009);
+    public static readonly ServiceDefinition Relationships = new(nameof(Relationships), "https", "localhost", 5010);
+    public static readonly ServiceDefinition Spheres = new(nameof(Spheres), "https", "localhost", 5011);
+    public static readonly ServiceDefinition Timeline = new(nameof(Timeline), "https", "localhost", 5012);
+    public static readonly ServiceDefinition SphereWebTestApp = new(nameof(SphereWebTestApp), "https", "localhost", 5013);
+
+    private static ConsulClient? client = null;
+    public static async Task<WriteResult> RegisterService(AgentServiceRegistration registration, CancellationToken token = default)
+    {
+        if (client is null)
+        {
+            client = new ConsulClient();
+        }
+
+
+        return await client.Agent.ServiceRegister(registration, token);
+    }
+
+    public static async Task<WriteResult> UnregisterService(AgentServiceRegistration registration, CancellationToken token = default)
+    {
+        return client is null
+            ? throw new Exception($"{registration.Name} service was never registered, or client was set to null")
+            : await client.Agent.ServiceDeregister(registration.ID, token);
+    }
 }
 
 public record ServiceDefinition(string Name, string Scheme, string Host, int Port)
---
+++ Sphere.Shared/SphericalLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Sphere.Shared;
+
+public static class SphericalLogger
+{
+    public static ILogger SetupLogger()
+    {
+        return new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
+    }
+
+    public static void ConfigureLogger(HostBuilderContext context, LoggerConfiguration configuration)
+    {
+        configuration
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(context.Configuration);
+    }
+}

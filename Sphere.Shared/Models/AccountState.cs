59fe3f36ab3047be40d9a4a9f9d0d2859f883d38
Chad Jessup <chad.jessup@gmail.com>
2022-07-11 01:03:57 -0700

    grain states

---
+++ Sphere.Shared/Models/AccountState.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Sphere.Shared.Models;
+
+[Serializable]
+public class AccountState
+{
+    [EmailAddress, Required(AllowEmptyStrings = false)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false)]
+    public string AccountDisplayName { get; set; } = string.Empty;
+
+    [Phone]
+    public string? PhoneNumber { get; set; }
+
+    public long AccountId { get; set; }
+    public CreatedWith CreatedWith { get; set; } = CreatedWith.Unknown;
+    public DateTime CreatedAt { get; set; }
+    public string? TimeZone { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public IPAddress? CreationIP { get; set; }
+}
+
+[Serializable]
+public enum CreatedWith
+{
+    Unknown = 0,
+    Web,
+    Mobile,
+}
---
+++ Sphere.Shared/Models/UserProfileState.cs
@@ -0,0 +1,7 @@
+﻿namespace Sphere.Shared.Models;
+
+[Serializable]
+public record UserProfileState(Description Description, string? AvatarMediaUrl, string? HeaderMediaUrl);
+
+[Serializable]
+public record Description(string? Bio, string? Pronouns, string? Website, string? Location);
